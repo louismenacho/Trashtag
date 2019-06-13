de01a92a39aed4bfcaba1022399b1411ae5b2d80
Louis Menacho <menacho.louis@gmail.com>
2019-06-12 21:23:36 -0400

    Initial Commit

---
+++ Trashtag/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Trashtag
+//
+//  Created by Louis Menacho on 6/12/19.
+//  Copyright © 2019 Louis Menacho. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Trashtag/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Trashtag
+//
+//  Created by Louis Menacho on 6/12/19.
+//  Copyright © 2019 Louis Menacho. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
